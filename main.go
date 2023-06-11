00252661532c981a557819cf2360db43d258e392
Hiroaki Nakamura <hnakamur@gmail.com>
2023-06-11 17:40:31 +0900

    Add graceful shutdown

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"flag"
 	"fmt"
 	"html"
 	"log"
 	"net/http"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/coreos/go-systemd/v22/activation"
+	"github.com/coreos/go-systemd/v22/daemon"
 )
 
 func main() {
@@ -17,6 +23,9 @@ func main() {
 }
 
 func run() error {
+	shutdownTimeout := flag.Duration("shutdown-timeout", 5*time.Second, "shutdown timeout")
+	flag.Parse()
+
 	listeners, err := activation.Listeners()
 	if err != nil {
 		return err
@@ -26,6 +35,19 @@ func run() error {
 	}
 
 	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		if wait := r.FormValue("wait"); wait != "" {
+			if d, err := time.ParseDuration(wait); err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				fmt.Fprintf(w, "invalid duration at \"wait\" query paramter.\n")
+				return
+			} else {
+				fmt.Fprintf(w, "Sleeping %s...\n", d)
+				if f, ok := w.(http.Flusher); ok {
+					f.Flush()
+				}
+				time.Sleep(d)
+			}
+		}
 		fmt.Fprintf(w, "Hello, %q\n", html.EscapeString(r.URL.Path))
 	})
 
@@ -34,5 +56,34 @@ func run() error {
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
-	return s.Serve(listeners[0])
+
+	idleConnsClosed := make(chan struct{})
+	var errShutdown error
+	go func() {
+		ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+		defer stop()
+
+		<-ctx.Done()
+
+		sent, err := daemon.SdNotify(false, daemon.SdNotifyStopping)
+		log.Printf("SdNotify stopping, sent=%v, err=%v", sent, err)
+
+		ctx, cancel := context.WithTimeout(context.Background(), *shutdownTimeout)
+		defer cancel()
+		errShutdown = s.Shutdown(ctx)
+		close(idleConnsClosed)
+	}()
+
+	sent, err := daemon.SdNotify(false, daemon.SdNotifyReady)
+	log.Printf("SdNotify ready, sent=%v, err=%v", sent, err)
+
+	err = s.Serve(listeners[0])
+	if err == http.ErrServerClosed {
+		err = nil
+	}
+	<-idleConnsClosed
+	if err != nil {
+		return errors.Join(err, errShutdown)
+	}
+	return nil
 }
